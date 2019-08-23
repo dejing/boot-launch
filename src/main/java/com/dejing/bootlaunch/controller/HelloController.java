2ffcf30e91802fe9d2ff973c9b2df699dc18e11b
dejing <66173469@qq.com>
2019-08-23 13:52:32 +0800

    idea结合Springboot开发技巧

--- src/main/java/com/dejing/bootlaunch/controller/HelloController.java
+++ src/main/java/com/dejing/bootlaunch/controller/HelloController.java
@@ -1,12 +1,22 @@
 package com.dejing.bootlaunch.controller;
 
+import com.dejing.bootlaunch.model.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello world!";
+    public Article hello(){
+
+//        Article article = new Article(1L, "dejing");
+//        article.setAuthor("dejing123");
+
+        Article article1 = Article.builder().id(2L).author("cqing").build();
+        log.info("test:" + article1);
+        return article1;
     }
 }
---
+++ src/main/java/com/dejing/bootlaunch/model/Article.java
@@ -0,0 +1,24 @@
+package com.dejing.bootlaunch.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Article {
+  private Long id;
+  private String author;
+  private String title;
+  private String content;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private Date createTime;
+  private List<Reader> reader;
+}
